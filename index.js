a2220db8560018208c50df24f770495a549f7d4f
Alys Scorpio <alyssad1182@gmail.com>
2021-08-03 12:54:21 -0700

    creating an array of questions

--- index.js
+++ index.js
@@ -3,7 +3,56 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Describe your app. What motivated you to create it?",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "How can users install your app?",
+        },
+        {
+            type: "input",
+            name: "links",
+            message: "At what links can users see the app?",
+        },
+        {
+            type: "input",
+            name: "contributors",
+            message: "Who contributed in the creation of your project?",
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "How did you test your app?",
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "What license(s) do you want to use?",
+            choices: ["MIT", "GNU GPLv3", "ISC"],
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub user name?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?",
+        },
+    ])
+    
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
